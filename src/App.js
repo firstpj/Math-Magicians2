e8ec8998dedeca866797a36d72528a6c2e31c11b
firstpj <114886262+firstpj@users.noreply.github.com>
2023-08-19 16:38:34 +0100

    src/pages, files

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
+import React from 'react';
+// eslint-disable-next-line import/no-extraneous-dependencies
+import { Route, Routes } from 'react-router-dom';
+import Home from './pages/Home';
+import QuotePage from './pages/QuotePage';
+import './App.css';
+import Layout from './Components/Layout/Layout';
 import Calculator from './Components/Calculator';
 
 function App() {
   return (
-    <div className="App">
-      <Calculator />
-    </div>
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route exact path="/" element={<Home />} />
+        <Route exact path="/Home" element={<Home />} />
+        <Route path="/calculator" element={<Calculator />} />
+        <Route path="/quote" element={<QuotePage />} />
+      </Route>
+    </Routes>
   );
 }
 
---
+++ src/Components/Layout/Layout.js
@@ -0,0 +1,21 @@
+// eslint-disable-next-line import/no-extraneous-dependencies
+import { Outlet } from 'react-router-dom';
+import Navigation from '../../pages/Navigation';
+
+function Layout() {
+  return (
+    <div>
+      <header className="app-header">
+        <h1>Math Magicians</h1>
+        <Navigation />
+      </header>
+
+      {/* This element will render either <DashboardMessages> when the URL is
+          "/messages", <DashboardTasks> at "/tasks", or null if it is "/"
+      */}
+      <Outlet />
+    </div>
+  );
+}
+
+export default Layout;
